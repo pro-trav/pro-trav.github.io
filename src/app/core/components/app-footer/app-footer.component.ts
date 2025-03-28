f19539dca87c7270ec8752891718235809db80eb
Changhui Xu <changhuixu@hotmail.com>
2025-03-28 18:39:56 -0500

    update: add some simple links

--- src/app/core/components/app-footer/app-footer.component.ts
+++ src/app/core/components/app-footer/app-footer.component.ts
@@ -1,17 +1,17 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './app-footer.component.html',
-    styleUrls: ['./app-footer.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    standalone: false
+  selector: 'app-footer',
+  templateUrl: './app-footer.component.html',
+  styleUrls: ['./app-footer.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: false,
 })
 export class AppFooterComponent implements OnInit {
   year = 0;
-  emailLinkText = 'Business Services IT';
-  emailDistributionList = 'FBIS-DL_Developer@iowa.uiowa.edu';
-  emailSubject = 'Questions about the OrderManager Website';
+  emailLinkText = 'protrav@uiowa.edu';
+  emailDistributionList = 'protrav@uiowa.edu';
+  emailSubject = 'Questions about ProTrav';
   emailHref = `mailto:${
     this.emailDistributionList
   }?subject=${this.emailSubject.replace(' ', '%20')}`;
